fb056eb352d3148fe036855647c5c25bd06b0f12
Rashmi Priya <priye98priya@gmail.com>
2024-02-19 15:14:22 +0530

    Initial commit

---
+++ ConAppPlayerListAssessment/ManagePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayerListAssessment
+{
+    public class ManagePlayer
+    {
+        private List<Player> players;
+
+        public ManagePlayer() 
+        { 
+            //Initialize the list of players.
+
+            players = new List<Player>();
+            {
+                new Player { Id = 1, Name = "Rashmi", Age = 23 };
+                new Player { Id = 2, Name = "Priya", Age = 24 };
+            };
+        }
+
+        public List<Player> GetPlayers()
+        {
+            return players;
+        }
+
+        public Player? GetPlayerById(int playerId)
+        {
+            return players.FirstOrDefault(p => p.Id == playerId);
+        }
+
+
+    }
+}
---
+++ ConAppPlayerListAssessment/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConAppPlayerListAssessment;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        ManagePlayer managePlayer = new ManagePlayer();
+        //get all players
+        Console.WriteLine("All Players: ");
+        var allPlayers = managePlayer.GetPlayers();
+        PrintPlayer(allPlayers);
+
+        //get player by id
+        int playerIdToFind = 1;
+        Console.WriteLine($"\n Player with Id {playerIdToFind}:");
+        var playerById = managePlayer.GetPlayerById(playerIdToFind);
+        PrintPlayer(playerById);
+    }
+
+    static void PrintPlayer(List<Player> players)
+    {
+        foreach ( var player in players )
+        {
+            PrintPlayer(player);
+        }
+    }
+
+    static void PrintPlayer(Player player)
+    {
+        if(player != null )
+        {
+            Console.WriteLine($"Id: {player.Id},Name:{player.Name},Age:{player.Age}");
+
+        }
+        else
+        {
+            Console.WriteLine("Player not found.");
+        }
+
+    }
+
+}
